7eacb139aa51a5c8bd8b2e8f13c1fed51da68ba2
Dymealves-Dev <73923367+Dymealves-Dev@users.noreply.github.com>
2021-03-31 18:06:56 -0300

    deploy website

--- src/config/database/connection.js
+++ src/config/database/connection.js
@@ -1,7 +1,7 @@
 import sequelize from "sequelize"
 
-export const connection = new sequelize("podpahapp", "root", "", {
-    host: "localhost",
+export const connection = new sequelize("podpahapp", "dymealves", "v]SQJ2[3vV[", {
+    host: "mysql743.umbler.com",
     dialect: "mysql",
     timezone: "-03:00"
 })
